b210c0a0db6f60e9fb71cd476aed5494716d5be0
kamp <lexiaky@gmail.com>
2026-02-25 20:15:10 -0500

    homepage 4 col

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -50,7 +50,7 @@ const HomePage = () => {
             {notes.length === 0 && <NotesNotFound />}
 
             {notes.length > 0 && (
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                     {notes.map(note => (
                       <NoteCard key={note._id} note={note} setNotes={setNotes}/>
                     ))}
