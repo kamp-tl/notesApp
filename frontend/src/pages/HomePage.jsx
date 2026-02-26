e3033b6e7459e62c5e05819bd1570b59ad1f3fba
kamp <lexiaky@gmail.com>
2026-02-25 20:16:36 -0500

    tryit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -44,13 +44,13 @@ const HomePage = () => {
     <div className="min-h-screen">
         <NavBar />
 
-        <div className="max-w-7xl mx0-auto p-4 mt-6">
+        <div className="max-w-9xl mx0-auto p-4 mt-6">
             {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
             {notes.length === 0 && <NotesNotFound />}
 
             {notes.length > 0 && (
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {notes.map(note => (
                       <NoteCard key={note._id} note={note} setNotes={setNotes}/>
                     ))}
